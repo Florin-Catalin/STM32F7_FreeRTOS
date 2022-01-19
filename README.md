b791a0ac8da7cdd292b1885ad334e72cdb245aa8
WinFlo <florin.g.ungureanu@gmail.com>
2022-01-19 04:13:51 +0200

    Misc
    cleanup + added the reference manual

---
+++ README.md
@@ -0,0 +1,8 @@
+FreeRTOS and STM32F767ZI with debugging
+---
+A self-learning project
+---
+Chapter 1 : [Use ARM Semi-hosting feature to print logs on the console](https://github.com/Florin-Catalin/STM32F7_FreeRTOS/tree/arm-semi-hosting-ex1)
+
+Chapter 2 : [Use UART peripheral of the MCU to print logs]( coming soon)
+
--- src/main.c
+++ src/main.c
@@ -8,47 +8,14 @@
   ******************************************************************************
 */
 
-#include <stdio.h>
-#include <stdint.h>
+
 #include "stm32f7xx.h"
 #include "stm32f7xx_nucleo_144.h"
-#include "FreeRTOS.h"
-#include "task.h"
-void vTask1_handler(void *params);
-void vTask2_handler(void *params);
-xTaskHandle xTaskHandle1 = NULL;
-xTaskHandle xTaskHandle2 = NULL;
-extern void initialise_monitor_handles();
+
+
 int main(void)
 {
-	initialise_monitor_handles();
-	printf("something");
-  HAL_DeInit();
-  SystemCoreClockUpdate();
-  xTaskCreate( vTask1_handler,
-		  "Task-1",
-		  configMINIMAL_STACK_SIZE,
-		  NULL,
-		  2,
-		  &xTaskHandle1);
-  xTaskCreate( vTask2_handler,
-		  "Task-2",
-		  configMINIMAL_STACK_SIZE,
-		  NULL,
-		  2,
-		  &xTaskHandle2);
-  vTaskStartScheduler();
+
 	for(;;);
 }
-void vTask1_handler(void *params){
- while(1)
- {
-	 printf("Hello-World: From Task-1\n");
- }
-}
-void vTask2_handler(void *params){
- while(1)
- {
-	 printf("Hello-World: From Task-2\n");
- }
-}
+
